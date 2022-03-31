5ee75d62273308e7e0e3db8f022f0ae5eafa9f4b
Patric Salvisberg <patric.salvisberg@gmail.com>
2022-03-31 13:28:58 +0200

    Deletion of unused Methods - close #65

--- src/main/java/ch/uzh/ifi/hase/soprafs22/rest/dto/UserGetDTO.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/rest/dto/UserGetDTO.java
@@ -32,10 +32,6 @@ public class UserGetDTO {
     this.username = username;
   }
 
-
-  public String getPassword(){
-      return password;}
-
   public void setPassword(String password){
       this.password = password;
     }
--- src/main/java/ch/uzh/ifi/hase/soprafs22/rest/dto/UserPutDTO.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/rest/dto/UserPutDTO.java
@@ -30,14 +30,11 @@ public class UserPutDTO {
         this.username = username;
     }
 
-    public String getPassword(){
-        return password;}
 
     public void setPassword(String password){
         this.password = password;
     }
 
-    public Date getDate(){return date;}
 
     public void setDate(Date date){this.date = date;}
 
