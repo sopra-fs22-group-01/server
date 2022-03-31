29581283793c7ceac47fd47dccbfdcf9c5d6a84c
Patric Salvisberg <patric.salvisberg@gmail.com>
2022-03-31 13:30:22 +0200

    added comment

--- src/main/java/ch/uzh/ifi/hase/soprafs22/constant/UserStatus.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/constant/UserStatus.java
@@ -2,7 +2,6 @@ package ch.uzh.ifi.hase.soprafs22.constant;
 
 //some comment to test git
 public enum UserStatus {
-  //OFFLINE = 0 in database
-  //ONLINE = 1 in database
+  //OFFLINE = 0 in database, ONLINE = 1 in database
   OFFLINE, ONLINE;
 }
