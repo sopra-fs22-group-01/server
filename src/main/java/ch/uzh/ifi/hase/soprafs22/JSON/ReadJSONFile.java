8640142d25fe1329b37461bcbb013699bfe360c3
SeyyidPalta <seyyidoekkes.palta@uzh.ch>
2022-04-01 08:51:39 +0200

    WARNING: This commit does not have a working code!
    I tried to import the JSON library but it somehow does not work. I am still working on it.

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/JSON/ReadJSONFile.java
@@ -0,0 +1,37 @@
+package ch.uzh.ifi.hase.soprafs22.JSON;
+
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import org.apache.tomcat.util.json.JSONParser;
+import org.apache.tomcat.util.json.ParseException;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.*;
+
+public class ReadJSONFile {
+    public static void main(String[] args) {
+        JSONParser parser = new JSONParser();
+
+        try (FileReader reader = new FileReader("cah-cards-full.json")){
+            Object obj = parser.parse(reader);
+            JSONArray a = (JSONArray) obj;
+            for(Object o: a){
+                JSONPObject card = (JSONPObject) o;
+                String text = (String) card.getValue();
+                System.out.println("Text: " + text);
+            }
+
+
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+}
