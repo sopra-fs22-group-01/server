1f8636ccc429c861cf8e9fb3b4a2dda62396aeef
lsenni <larissa.senning@uzh.ch>
2022-04-02 11:45:16 +0200

    Created Tests

---
+++ src/test/java/ch/uzh/ifi/hase/soprafs22/game/GameTest.java
@@ -0,0 +1,12 @@
+package ch.uzh.ifi.hase.soprafs22.game;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+    @Test
+    void getInstance() {
+    }
+}
