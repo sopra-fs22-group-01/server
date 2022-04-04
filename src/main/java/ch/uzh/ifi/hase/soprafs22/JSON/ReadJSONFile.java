96085b177ac17c8b0b78a6b31bb8f827849aca21
SeyyidPalta <seyyidoekkes.palta@uzh.ch>
2022-04-04 12:48:29 +0200

    implemented JSON reader and parser for cards
    not complete, relative path must be added for the file and creation of cards not implemented yet either

--- src/main/java/ch/uzh/ifi/hase/soprafs22/JSON/ReadJSONFile.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/JSON/ReadJSONFile.java
@@ -1,6 +1,9 @@
-package ch.uzh.ifi.hase.soprafs22.JSON;
+/**
+ * This class is merely for reading JSON Against Humanity file
+ * It creates cards instances out of JSON Objects
+ * */
 
-import org.apache.tomcat.util.json.ParseException;
+package ch.uzh.ifi.hase.soprafs22.JSON;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -9,36 +12,47 @@ import java.io.IOException;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class ReadJSONFile {
     public static void main(String[] args) {
+        // create a parser for JSON
         JSONParser parser = new JSONParser();
 
-        try (FileReader reader = new FileReader("cah-cards-full.json")){
+        // try to read the file, else, catch exceptions
+        try (FileReader reader = new FileReader("C:\\Users\\okkes\\SoPra-Group-01\\server\\src\\main\\java\\ch\\uzh\\ifi\\hase\\soprafs22\\JSON\\cah-cards-full.json")){
+            // create an object by parsing and convert the object to JSONArray
             Object obj = parser.parse(reader);
             JSONArray a = (JSONArray) obj;
+
+            // iterate over JSONArray
             for(Object o: a){
-                JSONObject card = (JSONObject) o;
-                String text = (String) card.get("text");
-                System.out.println("Text: " + text);
+                JSONObject cards = (JSONObject) o;
+
+                // for each white and black card dictionary, create another JSONArray to iterate
+                JSONArray whiteCards = (JSONArray) cards.get("white");
+                JSONArray blackCards = (JSONArray) cards.get("black");
+
+                // iterate over white cards
+                for(Object w: whiteCards){
+                    JSONObject whiteCard = (JSONObject) w;
+
+                    System.out.println(whiteCard.get("text"));
+                }
+
+                // iterate over black cards
+                for(Object b: blackCards){
+                    JSONObject blackCard = (JSONObject) b;
+                    System.out.println(blackCard.get("text"));
+                }
             }
-
-
         } catch (FileNotFoundException e){
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        catch (org.json.simple.parser.ParseException e) {
+        catch (ParseException e) {
             e.printStackTrace();
         }
     }
 }
-
-/*
-* json = response.json()
-* print(json['items'][0]['volumeInfo']['title'])
-* for x in range(nr_books):
-*   print(json['items'][x]['volumeInfo']['authors'])
-*
-* */
