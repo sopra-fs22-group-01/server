f9352902812becdf5d949874977f98e1ae814893
Patric Salvisberg <patric.salvisberg@gmail.com>
2022-04-07 15:21:31 +0200

    added rules endpoint + text reader to read rules.txt file.

--- src/main/java/ch/uzh/ifi/hase/soprafs22/service/GameService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/service/GameService.java
@@ -1,12 +1,25 @@
 package ch.uzh.ifi.hase.soprafs22.service;
 
+import ch.uzh.ifi.hase.soprafs22.entity.User;
 import ch.uzh.ifi.hase.soprafs22.repository.UserRepository;
+import ch.uzh.ifi.hase.soprafs22.rest.dto.UserGetDTO;
+import ch.uzh.ifi.hase.soprafs22.rest.mapper.DTOMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.io.File;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 @Service
 @Transactional
@@ -20,4 +33,19 @@ public class GameService {
     public GameService(@Qualifier("userRepository") UserRepository userRepository) {
         this.userRepository = userRepository;
     }
+
+    //reads Rules textfile from game/helpers/rules
+    public ArrayList<String> getRulesFromText() throws Exception{
+        ArrayList<String> ruleArrayList = new ArrayList<String>();
+
+        File file = new File("src/main/java/ch/uzh/ifi/hase/soprafs22/game/helpers/rules.txt");
+        Scanner sc = new Scanner(file);
+
+        while(sc.hasNextLine()){
+            ruleArrayList.add(sc.nextLine());
+        }
+
+        return ruleArrayList;
+    }
+
 }
