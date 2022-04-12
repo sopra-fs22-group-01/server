6ce46cb63de65f9a38270920228026deb278b8c5
lsenni <larissa.senning@uzh.ch>
2022-04-12 14:48:35 +0200

    Deleting of all Decks and the iterator Pattern

--- src/main/java/ch/uzh/ifi/hase/soprafs22/game/deck/IterableCard.java
+++
@@ -1,5 +0,0 @@
-package ch.uzh.ifi.hase.soprafs22.game.deck;
-
-public interface IterableCard {
-    IteratorCard iterator();
-}
--- src/main/java/ch/uzh/ifi/hase/soprafs22/game/deck/IteratorCard.java
+++
@@ -1,8 +0,0 @@
-package ch.uzh.ifi.hase.soprafs22.game.deck;
-
-import ch.uzh.ifi.hase.soprafs22.game.card.Card;
-
-public interface IteratorCard {
-    boolean hasNext();
-    Card next();
-}
