d87ad37d6e5dba13c341701f9e92ab490797db02
Patric Salvisberg <patric.salvisberg@gmail.com>
2022-04-14 21:17:17 +0200

    mock rules endpoint

--- src/main/java/ch/uzh/ifi/hase/soprafs22/game/Match.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/game/Match.java
@@ -52,11 +52,4 @@ public class Match {
         }
     }
 
-
-    //returns a copy of the current scoreboard
-    public ScoreBoard getScoreBoard(){
-        ScoreBoard scoreBoardCopy = new ScoreBoard();
-        scoreBoardCopy = this.scoreBoard;
-        return scoreBoardCopy;
-    }
 }
