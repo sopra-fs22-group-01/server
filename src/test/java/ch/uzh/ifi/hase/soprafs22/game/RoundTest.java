4ce150cf24ea6b51a0641a6bf3c1d3f6efc8031d
SeyyidPalta <seyyidoekkes.palta@uzh.ch>
2022-04-30 13:38:25 +0200

    added RoundTest

--- src/test/java/ch/uzh/ifi/hase/soprafs22/game/RoundTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs22/game/RoundTest.java
@@ -1,6 +1,7 @@
 package ch.uzh.ifi.hase.soprafs22.game;
 
 import ch.uzh.ifi.hase.soprafs22.entity.User;
+import ch.uzh.ifi.hase.soprafs22.game.card.BlackCard;
 import ch.uzh.ifi.hase.soprafs22.game.card.WhiteCard;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,17 +17,23 @@ class RoundTest {
 
     @BeforeEach
     void setUp() {
-        /*
-        testRound.startNewRound();
-        testWhiteCards = new ArrayList<>();*/
+        ArrayList<User> users = new ArrayList<>();
+        User user = new User();
+        users.add(user);
+        testRound = new Round(users);
+        testWhiteCards = new ArrayList<>();
     }
 
     @AfterEach
     void tearDown() {
+        testRound = null;
+        testWhiteCards = null;
     }
 
     @Test
     void testStartNewRound() {
+        boolean actual = testRound.startNewRound();
+        assertTrue(actual);
     }
 
     @Test
@@ -35,10 +42,11 @@ class RoundTest {
 
     @Test
     void testSetBlackCard() {
-    }
-
-    @Test
-    void testGetBlackCard() {
+        BlackCard blackCard = new BlackCard();
+        blackCard.createCard();
+        testRound.setBlackCard(blackCard);
+        BlackCard actual = testRound.getBlackCard();
+        assertEquals(blackCard, actual);
     }
 
     @Test
@@ -50,12 +58,16 @@ class RoundTest {
     }
 
     @Test
-    void testGetAllChosenCards() {
+    void testGetAllChosenCardsSizeNull() {
+        testRound.startNewRound();
+        ArrayList<WhiteCard> chosenCards = testRound.getAllChosenCards();
+        assertEquals(0,chosenCards.size());
+    }
+
+    @Test
+    void testGetRoundWinnerCardsNull() {
+        ArrayList<WhiteCard> actual = testRound.getRoundWinnerCards();
+        assertEquals(0, actual.size());
     }
 }
-/*
-    @Test
-    void testGetRoundWinnerNull() {
-        // have not written yet since the class is not ready to be tested
-        /*User actual = testRound.getRoundWinner();
-        assertNull(actual);*/
+
