32cab13b0fdb937898e4bd401e211b9411b89a2c
SeyyidPalta <seyyidoekkes.palta@uzh.ch>
2022-04-30 18:18:42 +0200

    deleted unused import statement

--- src/test/java/ch/uzh/ifi/hase/soprafs22/game/MatchTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs22/game/MatchTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-import java.util.Timer;
 
 import static org.junit.jupiter.api.Assertions.*;
 
