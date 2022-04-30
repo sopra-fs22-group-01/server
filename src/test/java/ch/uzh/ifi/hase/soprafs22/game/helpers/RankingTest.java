eb855ed4720b833a9dde6518a9bdd9b2f43b6eac
lsenni <larissa.senning@uzh.ch>
2022-04-30 19:07:45 +0200

    RankingTest finished, 100% coverage

--- src/test/java/ch/uzh/ifi/hase/soprafs22/game/helpers/RankingTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs22/game/helpers/RankingTest.java
@@ -7,36 +7,51 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RankingTest {
+    private Ranking ranking;
+    private String testUsername;
 
     @BeforeEach
     void setUp() {
+        testUsername = "";
+        this.ranking = new Ranking(1, testUsername, 0);
     }
 
     @AfterEach
     void tearDown() {
+        testUsername = null;
+        this.ranking = null;
     }
 
     @Test
     void getRank() {
+        assertEquals(1, ranking.getRank());
     }
 
     @Test
     void getUsername() {
+        assertEquals(testUsername, ranking.getUsername());
     }
 
     @Test
     void setRank() {
+        ranking.setRank(1);
+        assertEquals(1, ranking.getRank());
     }
 
     @Test
     void setUsername() {
+        ranking.setUsername("testUsername2");
+        assertEquals("testUsername2", ranking.getUsername());
     }
 
     @Test
     void setScore() {
+        ranking.setScore(1);
+        assertEquals(1, ranking.getScore());
     }
 
     @Test
     void getScore() {
+        assertEquals(0, ranking.getScore());
     }
 }
