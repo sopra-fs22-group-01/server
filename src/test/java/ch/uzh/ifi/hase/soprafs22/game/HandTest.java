3a0a1344bd12c67cb986df547d858ebb7c9df2b2
SeyyidPalta <seyyidoekkes.palta@uzh.ch>
2022-04-30 17:11:20 +0200

    added tests for hand

--- src/test/java/ch/uzh/ifi/hase/soprafs22/game/HandTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs22/game/HandTest.java
@@ -1,22 +1,61 @@
 package ch.uzh.ifi.hase.soprafs22.game;
 
+import ch.uzh.ifi.hase.soprafs22.entity.User;
+import ch.uzh.ifi.hase.soprafs22.game.card.WhiteCard;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class HandTest {
+    private Hand testHand;
+    private User testUser;
 
     @BeforeEach
     void setUp(){
+        testUser = new User();
+        testHand = new Hand(testUser);
+    }
 
+    @AfterEach
+    void tearDown(){
+        testUser = null;
+        testHand = null;
     }
 
     @Test
-    void createHand() {
+    void testCreateHand() {
+        testHand.createHand();
+        ArrayList<WhiteCard> actual = testHand.getUserHand();
+        assertEquals(10, actual.size());
     }
 
     @Test
-    void addingCard() {
+    void testUpdateHand(){
+        testHand.createHand();
+        WhiteCard testCard = testHand.getUserHand().get(0);
+        testHand.setChosenCard(testCard);
+        testHand.updateHand();
+        ArrayList<WhiteCard> actual = testHand.getUserHand();
+        assertEquals(10, actual.size());
+    }
+
+    @Test
+    void testResetChosenCard(){
+        testHand.createHand();
+        WhiteCard testCard = testHand.getUserHand().get(0);
+        testHand.setChosenCard(testCard);
+        testHand.resetChosenCard();
+        assertNull(testHand.getChosenCard());
+    }
+
+    @Test
+    void testGetCardsFromHand(){
+        testHand.createHand();
+        ArrayList<WhiteCard> whiteCards = testHand.getCardsFromHand();
+        assertEquals(10, whiteCards.size());
     }
 }
