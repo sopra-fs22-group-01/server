6f0e4d146f318d32d17354fe2a0697099372ef09
thphan21 <thuan.phan@uzh.ch>
2022-05-01 02:28:45 +0200

    wrote test for endpoint creating new match, doesnt work yet

--- src/test/java/ch/uzh/ifi/hase/soprafs22/controller/GameControllerTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs22/controller/GameControllerTest.java
@@ -1,5 +1,12 @@
 package ch.uzh.ifi.hase.soprafs22.controller;
 
+import ch.uzh.ifi.hase.soprafs22.constant.UserStatus;
+import ch.uzh.ifi.hase.soprafs22.entity.User;
+import ch.uzh.ifi.hase.soprafs22.exceptions.IncorrectIdException;
+import ch.uzh.ifi.hase.soprafs22.game.GameManager;
+import ch.uzh.ifi.hase.soprafs22.game.Lobby;
+import ch.uzh.ifi.hase.soprafs22.game.Match;
+import ch.uzh.ifi.hase.soprafs22.game.helpers.LobbyStatus;
 import ch.uzh.ifi.hase.soprafs22.service.GameService;
 import ch.uzh.ifi.hase.soprafs22.service.UserService;
 import org.junit.jupiter.api.Test;
@@ -15,8 +22,8 @@ import java.util.ArrayList;
 
 import static org.hamcrest.Matchers.is;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.mockito.Mockito.doNothing;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -28,6 +35,13 @@ public class GameControllerTest {
 
     @MockBean
     private GameService gameService;
+
+    @MockBean
+    private UserService userService;
+
+    @MockBean
+    private GameManager gameManager = GameManager.getInstance();
+
 
     @Test
     public void getRules_success() throws Exception{
@@ -42,4 +56,58 @@ public class GameControllerTest {
         mockMvc.perform(getRequest)
                 .andExpect(status().isOk());
     }
+
+
+    //create match successful --> check same Id, same players as lobby, matchStatus
+    @Test
+    public void givenLobbyId_createNewMatch_successful() throws Exception {
+        // simulate all steps before creating match
+
+        // create user
+        User user = new User();
+        user.setId(1L);
+        user.setUsername("testUsername");
+        user.setToken("1");
+        user.setUserStatus(UserStatus.ONLINE);
+        given(userService.createUser(Mockito.any())).willReturn(user);
+
+    // create lobby
+        Lobby lobby = new Lobby(2L);
+        given(gameService.createNewLobby()).willReturn(lobby);
+
+        long lobbyId = lobby.getId();
+
+        // put user in lobby
+        given(userService.findUserById(Mockito.any())).willReturn(user);
+        given(gameManager.getLobby(Mockito.any())).willReturn(lobby);
+        lobby.addPlayer(user);
+
+        // check if all users are ready -> has to wait (user hasnt changed ready status)
+        gameService.checkIfLobbyStatusChanged(lobbyId);
+        given(gameService.getLobbyStatus(Mockito.any())).willReturn(LobbyStatus.Waiting);
+
+        // make readyStatus of user -> READY
+        userService.updateUserReadyStatus(user);
+
+        given(gameManager.getLobby(Mockito.any())).willReturn(lobby);
+        gameService.updateUserReadyStatus(lobbyId, user.getId());
+
+        // check if all users are ready -> turn lobby into match
+        gameService.checkIfLobbyStatusChanged(lobbyId);
+        given(gameService.getLobbyStatus(Mockito.any())).willReturn(LobbyStatus.All_Ready);
+
+        // starting match should return this
+        Match match = new Match(lobbyId);
+        given(gameService.startMatch(Mockito.any())).willReturn(match);
+
+        // build request
+        MockHttpServletRequestBuilder postRequest = post("/matches/"+lobbyId);
+
+        // then perform request
+        mockMvc.perform(postRequest).andExpect(status().isOk());
+
+    }
+    //create match unsuccessful
+
+
 }
